c3901d618d136a5a0d96fae0f3291fe8640a56e8
Gurpal Bahra <gurpal_bahra@hotmail.co.uk>
2017-02-28 16:56:25 +0000

    turns out i did it wrong

--- back-end/public/js/factories/events.fact.js
+++ back-end/public/js/factories/events.fact.js
@@ -5,7 +5,7 @@ angular
 function eventsFactory(API, $http){
   return{
     get: function(){
-      return $http.get(API+'events/search/?api_key=abcdefghijklmnop&latitude=53.4839&longitude=-2.2446&radius=5&eventcode=LIVE&order=distance&description=1')
+
     }
   }
 }
