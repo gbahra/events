3b82db09239cabc62b5e81e88e0851ef3159b315
Gurpal Bahra <gurpal_bahra@hotmail.co.uk>
2017-02-28 10:19:48 +0000

    errors fixed and i am now changing states

--- back-end/public/js/app.js
+++ back-end/public/js/app.js
@@ -1,33 +1,34 @@
 angular
   .module('events',['ui.router'])
+  .constant('API', 'http://localhost:3000')
   .config(MainRouter)
-// .constant('API', 'http://localhost:3000')
+
 
 function MainRouter($stateProvider, $urlRouterProvider){
   $stateProvider
     .state('authRequired',{
       url: '/user/authRequired',
-      templateUrl: '/states/authRequired.html'
+      templateUrl: 'js/states/authRequired.html'
     })
     .state('new',{
       url: '/user/new',
-      templateUrl: '/states/newUser.html'
+      templateUrl: 'js/states/newUser.html'
     })
     .state('home',{
       url: '/home',
-      templateUrl: '/states/home.html'
+      templateUrl: 'js/states/home.html'
     })
     .state('searchResult',{
       url: '/event/:id',
-      templateUrl: '/states/searchResult.html'
+      templateUrl: 'js/states/searchResult.html'
     })
     .state('edit',{
       url: 'user/:id/edit',
-      templateUrl: '/states/editUser.html'
+      templateUrl: 'js/states/editUser.html'
     })
      .state('favourites',{
-      url: 'user/:id/favourites',
-      templateUrl: '/states/favourites.html'
+      url: '/favourites',
+      templateUrl: '/js/states/favourites.html'
     })
     $urlRouterProvider.otherwise('/')
 }
