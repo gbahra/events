5ec08dc82b2df913730cb45222cf74a0af5f431f
Gurpal Bahra <gurpal_bahra@hotmail.co.uk>
2017-02-28 17:12:44 +0000

    written more code to get stuff from api not tested but goes to backend now so beeter

--- back-end/config/routes.js
+++ back-end/config/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var usersController = require('../controllers/users');
+var eventsController = require('../controllers/events');
 
 //users
 router.route('/api/users')
@@ -11,5 +12,9 @@ router.route('/api/users/:id')
   .patch(usersController.update)
   .get(usersController.show)
   .delete(usersController.delete);
+
+//events
+router.route('api/events')
+  .get(eventsController.index);
 
 module.exports = router;
