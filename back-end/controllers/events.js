3decbce752be51ffb3664ac273983eb85d454fdd
Gurpal Bahra <gurpal_bahra@hotmail.co.uk>
2017-03-04 00:27:36 +0000

    code defo works query does not return artists in it so have keep trying till i get a query that does

--- back-end/controllers/events.js
+++ back-end/controllers/events.js
@@ -2,31 +2,35 @@ var request = require('request');
 
 function indexEvents(req, res){
   var finalObj  = {};
-  var increment = 0;
+  var increment = -1;
   request('http://www.skiddle.com/api/v1/events/search/?api_key=' + process.env.TOKENVARNAME + '&country=GB&order=trending&description=1&limit=12',
     function (error, response, body) {
       var parseBody = JSON.parse(response.body)
       for(var i = 0; i < parseBody.results.length; i++){
         secondReq(parseBody, i);
       }
+       // console.log(finalObj) //ASYNCHRONOUS
+
+
   })
 }
 
-function secondReq(parseBody, i, increment){
-  request('http://www.skiddle.com/api/v1/events/search/?api_key=' + process.env.TOKENVARNAME + "&city=" + parseBody.results[i].venue.town + "&limit=100", function(err, res, bod){
+function secondReq(parseBody, i, increment, finalObj){
+  request('http://www.skiddle.com/api/v1/events/search/?api_key=' + process.env.TOKENVARNAME + "&city=" + parseBody.results[i].venue.town + '&EventCode='+parseBody.results[i].EventCode +"&venue.id=" + parseBody.results[i].venueid +
+    "&limit=100", function(err, res, bod){
       var bodyOfCheck = JSON.parse(res.body)
       for(var j = 0; j < bodyOfCheck.results.length; j++){
         //console.log(parseBody.results[i].id, bodyOfCheck.results[j].id)
-        // '&EventCode='+parseBody.results[i].EventCode +"&venue.id=" + parseBody.results[i].venueid +
-
         if(parseBody.results[i].id === bodyOfCheck.results[j].id){
           console.log('yhhhhhhhhhhhh')
           var artists = [];
           increment++;
-          for(var k = 0; k< body.results[j].artists; k++){
+          console.log(bodyOfCheck.results[j])
+          for(var k = 0; k< bodyOfCheck.results[j].artists.length; k++){
             artists.push(body.results[j].artists[k])
           }
-          finalObj.increment = artists;
+          finalObj = {increment: artists};
+          console.log(finalObj)
         }
       }
   })
