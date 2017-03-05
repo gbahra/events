b55c47c375f3a79e236a5525745e623b2721093b
Gurpal Bahra <gurpal_bahra@hotmail.co.uk>
2017-03-05 16:18:06 +0000

    i am getting indexed results not sure how correct they are

--- back-end/controllers/events.js
+++ back-end/controllers/events.js
@@ -5,9 +5,10 @@ function indexEvents(req, res){
   var increment = -1;
   request('http://www.skiddle.com/api/v1/events/search/?api_key=' + process.env.TOKENVARNAME + '&country=GB&order=trending&description=1&limit=12',
     function (error, response, body) {
+      console.log('1st req')
       var parseBody = JSON.parse(response.body)
       for(var i = 0; i < parseBody.results.length; i++){
-        secondReq(parseBody, i);
+        secondReq(parseBody, i, increment, finalObj);
       }
        // console.log(finalObj) //ASYNCHRONOUS
 
@@ -16,21 +17,17 @@ function indexEvents(req, res){
 }
 
 function secondReq(parseBody, i, increment, finalObj){
-  request('http://www.skiddle.com/api/v1/events/search/?api_key=' + process.env.TOKENVARNAME + "&city=" + parseBody.results[i].venue.town + '&EventCode='+parseBody.results[i].EventCode +"&venue.id=" + parseBody.results[i].venueid +
-    "&limit=100", function(err, res, bod){//WRITE A QUERY THAT REUTRNS ARTISTS
+  request('http://www.skiddle.com/api/v1/events/search/?api_key=' + process.env.TOKENVARNAME + "&latitude=" + parseBody.results[i].venue.latitude + "&longitude=" + parseBody.results[i].venue.longitude + '&radius=100&order=distance&EventCode='+parseBody.results[i].EventCode +"&limit=100&description=1", function(err, res, bod){
       var bodyOfCheck = JSON.parse(res.body)
       for(var j = 0; j < bodyOfCheck.results.length; j++){
-        //console.log(parseBody.results[i].id, bodyOfCheck.results[j].id)
         if(parseBody.results[i].id === bodyOfCheck.results[j].id){
-          console.log('yhhhhhhhhhhhh')
           var artists = [];
           increment++;
-          // console.log(bodyOfCheck.results[j])
-          // for(var k = 0; k< bodyOfCheck.results[j].artists.length; k++){
-          //   artists.push(body.results[j].artists[k])
-          // }
+          for(var k = 0; k< bodyOfCheck.results[j].artists.length; k++){
+            artists.push(bodyOfCheck.results[j].artists[k])
+          }
           finalObj = {increment: artists};
-          // console.log(finalObj)
+          console.log(finalObj)
         }
       }
   })
